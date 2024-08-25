d0ecbc0fb9b99fe206f6f7d65a117a18020adfb2
Bekzodbekk <bekzodnematov709@gmail.com>
2024-08-26 00:40:30 +0500

    first

--- hotel-service/internal/repository/mongosh.go
+++ hotel-service/internal/repository/mongosh.go
@@ -2,8 +2,11 @@ package repository
 
 import (
 	"context"
-	"hotel-service/genproto/hotel"
+	pb "hotel-service/genproto/hotel"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -17,21 +20,68 @@ func NewHotelRepo(coll *mongo.Collection) HotelRepository {
 	}
 }
 
-func (h *HotelRepo) CreateHotel(ctx context.Context, req *hotel.CreateHotelReq) (*hotel.CreateHotelResp, error) {
+func (h *HotelRepo) CreateHotel(ctx context.Context, req *pb.CreateHotelReq) (*pb.CreateHotelResp, error) {
+	hotelReq := bson.M{
+		"name":        req.Name,
+		"location":    req.Location,
+		"rating":      req.Rating,
+		"address":     req.Address,
+		"rooms_count": 0,
+		"rooms":       bson.A{},
+		"created_at":  time.Now().String(),
+		"updated_at":  time.Now().String(),
+		"deleted_at":  0,
+	}
+
+	result, err := h.coll.InsertOne(ctx, hotelReq)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CreateHotelResp{
+		Status:  true,
+		Message: "Hotel created successfully",
+		Hotel: &pb.Hotel{
+			HotelId:    result.InsertedID.(string),
+			Name:       req.Name,
+			Location:   req.Location,
+			Rating:     req.Rating,
+			Address:    req.Address,
+			RoomsCount: 0,
+			Room:       &pb.Room{},
+			Cud: &pb.CUD{
+				CreatedAt: time.Now().String(),
+				UpdatedAt: time.Now().String(),
+				DeletedAt: 0,
+			},
+		},
+	}, nil
+}
+func (h *HotelRepo) UpdateHotel(ctx context.Context, req *pb.UpdateHotelReq) (*pb.UpdateHotelResp, error) {
+	hotelId, err := primitive.ObjectIDFromHex(req.)
+	hotelData := bson.M{
+		"$set": bson.M{
+			"name":        req.Name,
+			"location":    req.Location,
+			"rating":      req.Rating,
+			"address":     req.Address,
+			"rooms_count": 0,
+			"rooms":       bson.A{},
+			"updated_at":  time.Now().String(),
+		},
+	}
+	
 	return nil, nil
 }
-func (h *HotelRepo) UpdateHotel(ctx context.Context, req *hotel.UpdateHotelReq) (*hotel.UpdateHotelResp, error) {
+func (h *HotelRepo) DeleteHotel(ctx context.Context, req *pb.DeleteHotelReq) (*pb.DeleteHotelResp, error) {
 	return nil, nil
 }
-func (h *HotelRepo) DeleteHotel(ctx context.Context, req *hotel.DeleteHotelReq) (*hotel.DeleteHotelResp, error) {
+func (h *HotelRepo) GetHotels(ctx context.Context, req *pb.Void) (*pb.GetAllHotelsST, error) {
 	return nil, nil
 }
-func (h *HotelRepo) GetHotels(ctx context.Context, req *hotel.Void) (*hotel.GetAllHotelsST, error) {
+func (h *HotelRepo) GetHotelById(ctx context.Context, req *pb.GetHotelByIdReq) (*pb.GetHotelByIdResp, error) {
 	return nil, nil
 }
-func (h *HotelRepo) GetHotelById(ctx context.Context, req *hotel.GetHotelByIdReq) (*hotel.GetHotelByIdResp, error) {
-	return nil, nil
-}
-func (h *HotelRepo) GetHotelRoomsAvailability(ctx context.Context, req *hotel.HotelRoomsAvailabilityReq) (*hotel.HotelRoomsAvailabilityResp, error) {
+func (h *HotelRepo) GetHotelRoomsAvailability(ctx context.Context, req *pb.HotelRoomsAvailabilityReq) (*pb.HotelRoomsAvailabilityResp, error) {
 	return nil, nil
 }
